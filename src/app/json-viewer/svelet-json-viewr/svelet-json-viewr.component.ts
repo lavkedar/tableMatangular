4ef8184c00d75bb716ab8b6c4a5e7123da152c4e
lavkedar <lavkedar1298@gmail.com>
2024-08-29 01:17:52 +0530

    new changes

--- src/app/json-viewer/svelet-json-viewr/svelet-json-viewr.component.ts
+++ src/app/json-viewer/svelet-json-viewr/svelet-json-viewr.component.ts
@@ -1,6 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-// import { JSONEditor } from 'vanilla-jsoneditor'
-import JSONEditor from 'jsoneditor';
+import { JSONEditor } from 'vanilla-jsoneditor'
 
 @Component({
   selector: 'app-svelet-json-viewr',
@@ -9,35 +8,28 @@ import JSONEditor from 'jsoneditor';
 })
 export class SveletJsonViewrComponent {
 
-  @ViewChild('jsonEditorContainer', { static: true }) jsonEditorContainer!: ElementRef ;
-
-  editor: any;
   
-
-  getCurrentJsonData() {
-    return this.editor.get()
-    
-  }
-
-  ngAfterViewInit() {
-    const container = this.jsonEditorContainer.nativeElement;
-     this.editor = new JSONEditor(container, {
-      modes: ['tree', 'text'],
-      onChange: () => {
-
-        console.log('data changedc',this.getCurrentJsonData())
-        // Handle changes to the JSON data
-      }
-    });
-    this.editor.set({
-      // Initialize the JSON data
-      "name": "John",
-      "age": 30,
-      " occupation": "Developer"
-    });
-  }
-
+    @ViewChild('jsonEditorContainer', { static: true }) jsonEditorContainer!: ElementRef;
   
+    ngAfterViewInit() {
+      const container = this.jsonEditorContainer.nativeElement;
+      const editor = new JSONEditor({
+        target: container,
+        props: {
+          content: {
+            text: undefined,
+            json: {
+              "name": "John",
+              "age": 30,
+              "occupation": "Developer"
+            }
+          },
+          onChange: (updatedContent, previousContent, { contentErrors, patchResult }) => {
+            // Handle changes to the JSON data
+          }
+        }
+      });
+    }
 
 
 }
